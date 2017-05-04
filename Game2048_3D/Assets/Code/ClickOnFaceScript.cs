0d5953ebb147150c91ed8e63ebb4b6878c0c70d6
Dmitry Petrichenko <d.petrichenko@lucky-labs.com>
2017-05-04 18:08:48 +0300

    add Project

---
+++ Game2048_3D/Assets/Code/ClickOnFaceScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnFaceScript : MonoBehaviour {
+    // Значения публичных полей можно поменять прямо из редактора
+    // Хранит смещение, требуемое для расчета позиции нового объекта
+    public Vector3 delta;
+
+    // Эта функция вызывается, когда курсор находится над GameObject, на котором этот скрипт расположен
+    void OnMouseOver()
+    {
+        // Если нажата левая клавиша мыши
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Если нажата левая клавиша мыши
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Выводим сообщение в консоль
+                Debug.Log("Left click!");
+                // Уничтожаем блок, по которому кликнули
+                Destroy(this.transform.parent.gameObject);
+            }
+        }
+
+        // Если правая клавиша нажата
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Выводим сообщение в консоль
+            Debug.Log("Right click!");
+            // Вызываем метод из класса WorldGenerator
+            WorldGenerator.CloneAndPlace(this.transform.parent.transform.position + delta, // N = C + delta
+                                         this.transform.parent.gameObject); // Родительский GameObject
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Game2048_3D/Assets/Code/WorldGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void CloneAndPlace(Vector3 newPosition, GameObject originalGameobject)
+    {
+        // Клон
+        GameObject clone = (GameObject)Instantiate(originalGameobject,
+                                                   newPosition,
+                                                   Quaternion.identity);
+        // Позиция
+        clone.transform.position = newPosition;
+        // Переименовывем
+        clone.name = "Voxel@" + clone.transform.position;
+    }
+}

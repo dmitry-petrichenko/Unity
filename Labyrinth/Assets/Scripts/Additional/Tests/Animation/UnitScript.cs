db11d6d9ed876c8f688019f7223eda06473da064
Unknown <dima.petrichenko@gmail.com>
2018-01-05 22:33:35 +0200

    update

--- Labyrinth/Assets/Scripts/Additional/Tests/Animation/UnitScript.cs
+++ Labyrinth/Assets/Scripts/Additional/Tests/Animation/UnitScript.cs
@@ -10,16 +10,22 @@ public class UnitScript : MonoBehaviour
 
 	public void playWalkingAnimation()
 	{
-		_animator.SetBool("IsWalking", true);
+		//_animator.SetBool("IsWalking", true);
+		//_animator.Play("Move", -1, 0f);
+		_animator.SetInteger("animation", 15);
 	}
 	
 	public void playIdleAnimation()
 	{
-		_animator.SetBool("IsWalking", false);
+		//_animator.Play("Idle", -1, 0f);
+		_animator.SetInteger("animation", 2);
+		//_animator.SetBool("IsWalking", false);
 	}
 	
 	public void playAttackAnimation()
 	{
-		_animator.SetBool("IsAttacking", true);
+		_animator.SetInteger("animation", 11);
+		//_animator.Play("ATK1", -1, 0f);
+		//_animator.SetBool("IsAttacking", true);
 	}
 }
